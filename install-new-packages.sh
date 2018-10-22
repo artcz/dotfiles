139334dfab9f90ee15a097b6f1664305d902c932
Artur Czepiel <czepiel.artur@gmail.com>
2018-10-22 11:06:27 +0200

    added install-new-packages.sh

---
+++ install-new-packages.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+apt install \
+	openssh-server \
+	screen \
+	htop \
+	ncdu \
+	zsh \
+	git \
+	makepasswd \
+	sqlite3 \
+	python-pip \
+	python3-pip \
+	silversearcher-ag \
+	ack-grep \
+	docker-compose \
+	postgresql-client \
+	rxvt-unicode \
+	neovim \
+	konsole \
+	feh \
+	mpv \
+	syncthing \
+	awesome \
+	awesome-extra \
+	dolphin \
+	volumeicon-alsa \
+	clementine \
+	slock \
+	xss-lock \
+	arandr \
+	meld \
+	neovim-qt \
+	zeal \
+	dia \
+	gimp \
+	telegram-desktop \
+	kde-spectacle \
+	gwenview \
+	whois \
+
+pip2 install pip --upgrade
+pip3 install pip --upgrade
